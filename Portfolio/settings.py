d748618cd222eb7e3126aac5b89f1e6a0587e0be
Son-of-Grace-001 <horlharmighty2000@gmail.com>
2023-09-29 03:39:44 -0700

    remove whitenoise

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -130,7 +130,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
