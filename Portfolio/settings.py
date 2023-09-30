90d5ce8888de5379cace85dd64c6168b76380994
Son-of-Grace-001 <horlharmighty2000@gmail.com>
2023-09-29 18:52:37 -0700

    cloudinary set as default file storage

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -132,6 +132,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUD_NAME'),
